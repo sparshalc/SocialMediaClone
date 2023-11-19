2d78422fe02b81df652a912f051c636e596c8e95
“sparshalc” <imailsparsha@gmail.com>
2023-11-19 12:54:23 +0545

    Add Scroll to Comments

--- app/javascript/controllers/focus_comment_controller.js
+++ app/javascript/controllers/focus_comment_controller.js
@@ -1,11 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    this.element.setAttribute('data-action', 'click->focus-comment#click')
+    this.element.setAttribute('data-action', 'click->focus-comment#click');
   }
-  click(){
-    const commentForm = document.getElementById('comment_form')
-    commentForm.form[0].form[1].focus()
+
+  click() {
+    const commentForm = document.getElementById('comment_form');
+    commentForm.form[0].form[1].focus();
+    
+    const comments = document.getElementById('comment');
+    comments.classList.add('active');
+    comments.scrollIntoView({ behavior: 'smooth' });
+
+    setTimeout(() => {
+      comments.classList.remove('active');
+    }, 1000);
   }
 }
