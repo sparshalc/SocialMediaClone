e03aca96a235fff50519b41ab397c0f6cb24e840
“sparshalc” <imailsparsha@gmail.com>
2023-11-24 23:53:28 +0545

    Added Test Cases for model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,34 @@
-require "test_helper"
-
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "When a new comment is created" do
+    it "saves the comment to the database" do
+      user = User.new(email: 'imailsparsha@gmail.com', password: 'password', password_confirmation: 'password')
+      user.save
+      post = Post.new(title: 'Hello World', user_id: user.id, reposted_from: false)
+      post.save
+      comment = Comment.new(user_id: user.id, post_id: post.id, title: 'Comment on Hello World')
+      expect { comment.save }.to change(Comment, :count).by(1)
+    end
+  end
+  context "When a comment without title is created" do
+    it "doesn't saves the comment to the database" do
+      user = User.new(email: 'imailsparsha@gmail.com', password: 'password', password_confirmation: 'password') 
+      user.save
+      post = Post.new(title: 'Hello World', user_id: user.id, reposted_from: false)
+      post.save
+      comment = Comment.new(user_id: user.id, post_id: post.id)
+      expect { comment.save }.to change(Comment, :count).by(0)
+    end
+  end
+  context "When a comment is destroyed" do
+    it "gets destroyed from the database" do 
+      user = User.new(email: 'imailsparsha@gmail.com', password: 'password', password_confirmation: 'password')
+      user.save
+      post = Post.new(title: 'Hello World', user_id: user.id, reposted_from: false)
+      post.save
+      comment = Comment.new(user_id: user.id, post_id: post.id, title: 'Comment on Hello World')
+      expect { comment.save }.to change(Comment, :count).by(1)
+      comment.destroy
+      expect(Comment.count).to eq(0)
+    end
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,36 @@
-require "test_helper"
-
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ context "When a post title is present" do
+  it "should save post to the database" do
+    user = User.new(email: 'imailsparsha@gmail.com', password: 'password', password_confirmation: 'password' )
+    user.save
+    post = Post.new(title: "Hello World", user_id: user.id, reposted_from: false)    
+    expect { post.save }.to change(Post, :count).by(1)
+  end
+ end
+ context "When a post title is edited" do 
+  it "should update the title" do 
+    user = User.new(email: 'imailsparsha@gmail.com', password: 'password', password_confirmation: 'password' )
+    user.save
+    post = Post.new(title: "Hello World", user_id: user.id, reposted_from: false)    
+    post.save
+    post.title = "Testing"
+    post.save
+    expect(post.title).to eq("Testing")
+  end
+ end
+ context "When a user is absent while creating a post" do
+  it "shouldn't save the post to the database" do 
+    post = Post.new(title: 'Hello World', body: 'Test Case', reposted_from: false)
+    expect { post.save }.to change(Post, :count).by(0)
+  end
+ end
+ context "When a post is destroyed" do
+  it "should destroy post from the database" do 
+    user = User.new(email: 'imailsparsha@gmail.com', password: 'password', password_confirmation: 'password') 
+    user.save
+    post = Post.new(title: 'Hello World', user_id: user.id , reposted_from: false)
+    post.destroy
+    expect(Post.count).to eq 0
+  end
+ end
 end
